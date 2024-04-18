37ed0f2b125199d450576ac7f2f02b493407c92e
Lil-T3ikk <113462815+Lil-T3ikk@users.noreply.github.com>
2024-04-18 08:45:54 +0200

    pull try

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("push does work");
+        System.out.println("push does works");
+        System.out.println("pull does works too");
     }
 }
