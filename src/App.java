e9210000d3d51d8e591a999c173ac16501094eb4
Lil-T3ikk <113462815+Lil-T3ikk@users.noreply.github.com>
2024-04-18 08:47:35 +0200

    open with no pull

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("push does works");
-        System.out.println("pull does works too");
+        System.out.println("opened from github repository");
     }
 }
